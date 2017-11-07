6fa6edf8a0d7a58b7c24cf1da0f4e67cb9a1013a
Sergey Kichuk <skichuk@luxoft.com>
2017-11-08 02:42:25 +0300

    first commit

---
+++ LoadBalancer.Tests/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LoadBalancer.Tests
+{
+    [TestFixture]
+    public class LoadBalancerTests
+    {
+        [Test]
+        public void TestName()
+        {
+            var queue = new BlockingCollection<int>();
+
+            var producers = Enumerable.Range(0, 3)
+                .Select(x => Task.Factory.StartNew(() =>
+                {
+                    Enumerable.Range(1, 100).ToList()
+                        .ForEach(i => queue.Add(i));
+                    Thread.Sleep(100);
+
+                })).ToArray();
+
+            var consumres = Enumerable.Range(0, 3)
+               .Select(x => Task.Factory.StartNew(() =>
+               {
+                   foreach (var item in queue.GetConsumingEnumerable())
+                   {
+                       Console.WriteLine(item);
+                   }
+                   Thread.Sleep(100);
+
+               })).ToArray();
+
+            Task.WaitAll(producers);
+            Task.WaitAll(consumres);
+        }
+    }
+}
